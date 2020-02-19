125a62addac5b02745107281814dce7c276ab12b
Michele Locati <michele@locati.it>
2020-02-19 10:12:48 +0100

    Allow using custom commands with "docker run"

--- base/assets/entrypoint.sh
+++ base/assets/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-ccm-service start && bash
+if ! ccm-service start; then
+    echo 'Failed to start services' >&2
+    ccm-service stop >/dev/null 2>&1
+    exit 1
+fi
+
+if test $# -eq 0; then
+    bash
+    rc=$?
+else
+    bash -c "$*"
+    rc=$?
+fi
+
 ccm-service stop
+exit $rc
