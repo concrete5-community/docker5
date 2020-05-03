bd052181bd2467e75351375dc76dc0cb28d8e7cd
Michele Locati <michele@locati.it>
2020-05-03 14:51:22 +0200

    Refactoring

--- base/assets/entrypoint.sh
+++ base/assets/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if test -n "${CCM_PHP_VERSION:-}"; then
+    if ! switch-php "$CCM_PHP_VERSION"; then
+        exit 1
+    fi
+fi
+
 if ! ccm-service start; then
     echo 'Failed to start services' >&2
     ccm-service stop >/dev/null 2>&1
